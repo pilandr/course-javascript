7217479f4c766e4cb5e22704250633be734b3fa7
Andrei Piliushenko <pionbox@gmail.com>
2021-07-04 19:52:27 +0300

    5-async done

--- projects/async/index.js
+++ projects/async/index.js
@@ -39,7 +39,11 @@ const homeworkContainer = document.querySelector('#app');
  Массив городов пожно получить отправив асинхронный запрос по адресу
  https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
  */
-function loadTowns() {}
+function loadTowns() {
+  return fetch('https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json')
+    .then((res) => res.json())
+    .then((towns) => towns.sort((a, b) => a.name.localeCompare(b.name)));
+}
 
 /*
  Функция должна проверять встречается ли подстрока chunk в строке full
@@ -52,7 +56,10 @@ function loadTowns() {}
    isMatching('Moscow', 'SCO') // true
    isMatching('Moscow', 'Moscov') // false
  */
-function isMatching(full, chunk) {}
+function isMatching(full, chunk) {
+  if (full.toUpperCase().indexOf(chunk.toUpperCase()) >= 0) return true;
+  return false;
+}
 
 /* Блок с надписью "Загрузка" */
 const loadingBlock = homeworkContainer.querySelector('#loading-block');
@@ -67,8 +74,45 @@ const filterInput = homeworkContainer.querySelector('#filter-input');
 /* Блок с результатами поиска */
 const filterResult = homeworkContainer.querySelector('#filter-result');
 
-retryButton.addEventListener('click', () => {});
+let arr;
 
-filterInput.addEventListener('input', function () {});
+retryButton.addEventListener('click', () => {
+  loadPage();
+});
+
+filterInput.addEventListener('input', function () {
+  while (filterResult.firstChild) {
+    filterResult.removeChild(filterResult.firstChild);
+  }
+  if (filterInput.value === '') return;
+  for (let i = 0; i < arr.length; i++) {
+    if (isMatching(arr[i].name, this.value)) {
+      const div = document.createElement('div');
+      div.textContent = arr[i].name;
+      filterResult.append(div);
+    }
+  }
+});
+
+function loadPage() {
+  loadingBlock.style.display = 'block';
+  loadingFailedBlock.style.display = 'none';
+  filterBlock.style.display = 'none';
+  loadTowns().then(
+    (towns) => {
+      arr = towns;
+      loadingBlock.style.display = 'none';
+      filterBlock.style.display = 'block';
+      return towns;
+    },
+    (towns) => {
+      arr = towns;
+      loadingBlock.style.display = 'none';
+      loadingFailedBlock.style.display = 'block';
+      return towns;
+    }
+  );
+}
+loadPage();
 
 export { loadTowns, isMatching };
